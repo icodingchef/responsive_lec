54fb9ce989879e2b5237461e3517ca426d5ecdbb
icodingchef <icodingchef@gmail.com>
2022-08-18 11:03:05 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'mypage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            fontSize: 30,
+            color: Colors.white
+          )
+        )
+      ),
+      home: MyPage(),
+    );
+  }
+}
